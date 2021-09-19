128f730de905f48b5f155e954e9e1b6393f0ac58
Stefan van der Lugt <stefanvdlugt@gmail.com>
2021-09-19 21:59:23 +0200

    Use UUID-like primary keys

--- app/models.py
+++ app/models.py
@@ -2,8 +2,19 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+from time import time as time_
+from os import urandom
+
+def keygen():
+    '''
+    Returns a 16 bytes string.
+    The first 6 bytes give a timestamp (# milliseconds since 1 Jan 1970),
+    and the last 10 bytes are random.
+    '''
+    return int(time_()*1000).to_bytes(6,byteorder='big') + urandom(10)
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BINARY(length=16), primary_key=True, default=keygen)
     username = db.Column(db.String(32), index=True, unique=True)
     email = db.Column(db.String(256), index=True, unique=True)
     password_hash = db.Column(db.String(128))
@@ -17,6 +28,10 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def get_id(self):
+        return self.id.hex()
+
 @login.user_loader
 def load_user(id):
-    return User.query.get(int(id))
+    print((id, bytes.fromhex(id)))
+    return User.query.get(bytes.fromhex(id))
