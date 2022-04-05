2d27698c66a54a0761bb88f58b036a3990cd77b6
Stefan van der Lugt <git@svdlugt.nl>
2022-04-05 13:12:12 +0200

    Disable reminders by email if mail server is not setup.

--- app/tasks.py
+++ app/tasks.py
@@ -6,11 +6,11 @@ from datetime import datetime
 @scheduler.task("interval", id="send_reminders", seconds=20)
 def send_reminders():
     with db.app.app_context():
-        for reminder in Reminder.query.filter(
-            Reminder.sent == False,
-            Reminder.time < datetime.utcnow()
-        ):
-            send_reminder_email(reminder)
-            reminder.sent=True
-            db.session.commit()
-            
+        if db.app.config['MAIL_ENABLED']:
+            for reminder in Reminder.query.filter(
+                Reminder.sent == False,
+                Reminder.time < datetime.utcnow()
+            ):
+                send_reminder_email(reminder)
+                reminder.sent=True
+                db.session.commit()
